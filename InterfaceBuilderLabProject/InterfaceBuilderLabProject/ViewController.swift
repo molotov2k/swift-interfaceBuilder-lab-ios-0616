cf1d5a45670430c75e6c1719c18782d5d9b28d5e
molotov2k <molotov2k@gmail.com>
2016-07-07 16:44:45 -0400

    done

---
+++ InterfaceBuilderLabProject/InterfaceBuilderLabProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InterfaceBuilderLabProject
+//
+//  Created by Max Tkach on 7/7/16.
+//  Copyright © 2016 Anvil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
